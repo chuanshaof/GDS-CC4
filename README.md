c6bd4302f65c9b173a00b4d1e877c2ae1f108602
chuanshaof <chuanshaof@gmail.com>
2022-10-06 11:58:15 -0700

    task 2 complete, missing edge case tests

--- README.md
+++ README.md
@@ -3,4 +3,25 @@
 # Task 1
 restaurant_data_bugged is missing keys in the dictionary, such as "city"
 
-There is some issues with a country_id of 11, where the city is dummy and the ratings are 0. Strange data that might want to be removed.
+Assumptions:
+    1. 
+
+Comments on data:
+    1. There are some issues with a country_id of 17, where the city is dummy and the ratings are 0. Strange data that might want to be removed.
+
+Testing:
+    1. restaurant_data_edited is a downloaded JSON file with a mising key in the dictionary for "city". This is to ensure that files will be able to be read even if some data is missing. These data are then later filled with NA if they are missing.
+    2. 
+
+# Task 2
+Task 2 checks for the datetime of the system. In particular, for the month of April 2019. Assumption is to gather all events that have at least ONE hour of event in the month of April 2019.
+
+ACCEPTED EVENTS:
+    1. If start/end dates are in the month of April
+    2. If event starts before the 1st of April AND ends after the 1st of April
+
+Assumptions:
+    1. All inputted items from the JSON file is copied exactly and nothing is changed.
+    2. There are multiple pictures from a single event that is presented as a list.
+    3. Even if the event was a second in the month of April 2019, it will be in the list.
+    
--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 import pandas as pd
-import json, sys, csv
+import json, sys, csv, datetime
 
 def main():
     # https://stackoverflow.com/questions/71459981/how-to-solved-charmap-codec-cant-encode-character-u300b
@@ -10,21 +10,16 @@ def main():
     # ----------------- Reading JSON file -----------------
     # Change url here to what is necessary
     url = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
-    
-    # # https://pythonbasics.org/pandas-json/
-    # # 1. Reading json file using pandas
-    # df = pd.read_json(url)
-    # rawData = df.to_dict()
 
-    # # 2. Read JSON file from local file directory
-    # with open("restaurant_data_bugged.json", "r", encoding="utf-8") as f:
-    #     rawData = json.load(f)
-    
-    # 3. Read JSON file from url
+    # Read JSON file from url
     # https://www.geeksforgeeks.org/how-to-read-a-json-response-from-a-link-in-python/
     with urlopen(url) as response:
         rawData = json.loads(response.read().decode("utf-8"))
-    
+
+    # Reading date time
+    # https://www.geeksforgeeks.org/converting-string-yyyy-mm-dd-into-datetime-in-python/
+    dateTimeFormat = "%Y-%m-%d"
+
     events = []
     for data in rawData:
         for restaurant in data["restaurants"]:
@@ -44,26 +39,25 @@ def main():
 
                 for photo in event.get("photos", []):
                     photoURLList.append(photo.get("photo", {}).get("url", ""))
-                
-                events.append([eventId, restaurantId, restaurantName, photoURLList, eventTitle, eventStart, eventEnd])
+
+                dtEventStart = datetime.datetime.strptime(eventStart, dateTimeFormat)
+                dtEventEnd = datetime.datetime.strptime(eventEnd, dateTimeFormat)
+
+                apr1 = datetime.datetime(2019, 4, 1)
+                apr30 = datetime.datetime(2019, 4, 30)
+
+                if dtEventStart >= apr1 and dtEventStart <= apr30:
+                    events.append([restaurantId, restaurantName, eventId, eventTitle, eventStart, eventEnd, photoURLList])
+                elif dtEventEnd >= apr1 and dtEventEnd <= apr30:
+                    events.append([restaurantId, restaurantName, eventId, eventTitle, eventStart, eventEnd, photoURLList])
+                elif dtEventStart <= apr1 and dtEventEnd >= apr30:
+                    events.append([restaurantId, restaurantName, eventId, eventTitle, eventStart, eventEnd, photoURLList])
+                else:
+                    continue
 
     eventsDf = pd.DataFrame(events, columns=["event_id", "res_id", "res_name", "photos", "event_title", "start_date", "end_date"])
     eventsDf.replace("", "NA", inplace=True)
     eventsDf.to_csv("task2.csv", index=False)
-    
-
-    # restaurantsDf = pd.DataFrame(events, columns=["eventId", "name", "country", "city", "votes", "rating", "cuisines"])
-    # restaurantsDf.replace("", "NA", inplace=True)
-    # restaurantsDf.to_csv("task1.csv", index=False)
-
-    # # Note that there may be multiple events
-    # print(rawData[0]["restaurants"][0]["restaurant"]["zomato_events"][0]["event"]["event_id"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["id"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["name"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["zomato_events"][0]["event"]["photos"][0]["photo"]["url"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["zomato_events"][0]["event"]["title"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["zomato_events"][0]["event"]["start_date"])
-    # print(rawData[0]["restaurants"][0]["restaurant"]["zomato_events"][0]["event"]["end_date"])
 
 if __name__ == "__main__":
     main()

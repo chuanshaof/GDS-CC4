b607759a669384509fd13f4fcf64d65a5a39bc03
chuanshaof <chuanshaof@gmail.com>
2022-10-05 17:46:30 -0700

    task 1 complete, missing refinement

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# GDS-CC4" 
+
+# Task 1
+restaurant_data_bugged is missing keys in the dictionary, such as "city"
+
+There is some issues with a country_id of 11, where the city is dummy and the ratings are 0. Strange data that might want to be removed.
--- task1.py
+++ task1.py
@@ -1,33 +1,65 @@
 from urllib.request import urlopen
+import pandas as pd
 import json, sys, csv
 
 def main():
     # https://stackoverflow.com/questions/71459981/how-to-solved-charmap-codec-cant-encode-character-u300b
+    sys.stdin.reconfigure(encoding='utf-8')
     sys.stdout.reconfigure(encoding='utf-8')
 
     # Change url here to what is necessary
     url = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
 
-    # https://www.geeksforgeeks.org/how-to-read-a-json-response-from-a-link-in-python/
-    with urlopen(url) as response:
-        data = json.loads(response.read().decode("utf-8"))
+    # Reading csvfile using pandas and converting it into a dictionary
+    # Format will be {country_code: country_name}
+    countries = {}
+    countryDf = pd.read_excel("Country-Code.xlsx")
+    for i in countryDf.index:
+        countries[countryDf["Country Code"][i]] = countryDf["Country"][i]
 
-    print(len(data[0]["restaurants"]))
-    print(data[0]["restaurants"][0]["restaurant"]["R"]["res_id"])
-    print(data[0]["restaurants"][0]["restaurant"]["name"])
-    print(data[0]["restaurants"][0]["restaurant"]["location"]["country_id"])
-    print(data[0]["restaurants"][0]["restaurant"]["location"]["city"])
-    print(data[0]["restaurants"][0]["restaurant"]["user_rating"]["aggregate_rating"])
-    print(data[0]["restaurants"][0]["restaurant"]["cuisines"])
+    # # https://pythonbasics.org/pandas-json/
+    # # Reading json file using pandas
+    # df = pd.read_json(url)
+    # rawData = df.to_dict()
 
-    # https://docs.python.org/3/library/csv.html
-    with open('task1.csv', 'w') as file:
-        writer = csv.writer(file, lineterminator='\n')
+    # Read JSON file from local file directory
+    with open("restaurant_data.json", "r", encoding='utf-8') as f:
+        rawData = json.load(f)
+    
+    # # https://www.geeksforgeeks.org/how-to-read-a-json-response-from-a-link-in-python/
+    # with urlopen(url) as response:
+    #     rawData = json.loads(response.read().decode("utf-8"))
+        
+    restaurants = []
+    # Loading the data as rows
+    for data in rawData:
+        for restaurant in data["restaurants"]:
+            restaurant = restaurant["restaurant"]
+            
+            # Meant to handle missing dictionary keys
+            # https://docs.python.org/3/library/stdtypes.html#dict.get
+            id = restaurant.get("R", {}).get("res_id", "")
+            name = restaurant.get("name", "")
+            country = countries.get(restaurant.get("location", {}).get("country_id", ""), "")
+            city = restaurant.get("location", {}).get("city", "")
+            userRatingVotes = restaurant.get("user_rating", {}).get("votes", "")
+            userRating = restaurant.get("user_rating", {}).get("aggregate_rating", "")
+            cuisines = restaurant.get("cuisines", "")
 
-        # Write the header of the file
-        writer.writerow(["res_id", "name", "country_id", "city", "aggregate_rating", "cuisines"])
+            restaurants.append([id, name, country, city, userRatingVotes, userRating, cuisines])
 
-        writer.writerows(data) 
+    restaurantsDf = pd.DataFrame(restaurants, columns=["res_id", "name", "country", "city", "votes", "rating", "cuisines"])
+    restaurantsDf.replace("", "NA", inplace=True)
+
+    restaurantsDf.to_csv("task1.csv", index=False)
+
+    # # https://docs.python.org/3/library/csv.html
+    # with open('task1.csv', 'w', encoding="utf-8") as file:
+    #     writer = csv.writer(file, lineterminator='\n')
+
+    #     # Write the header of the file
+    #     writer.writerow(["res_id", "name", "country", "city", "aggregate_rating", "cuisines"])
+    #     writer.writerows(restaurantRows) 
 
 
 if __name__ == "__main__":
